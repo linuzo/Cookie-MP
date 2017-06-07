af285269d976d6e90252c0d00b709926977290f5
CookieCode <etienne.crosnier.la@gmail.com>
2017-06-08 01:03:11 +0200

    1.1.0.55 New Blocks

---
+++ src/pocketmine/block/PurpleGlazedTerracotta.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ *  _____   ______   ______   _  _   _   ______      __  __ ____
+ * |  _ _| |  __  | |  __  | | |/ / |_| |  ____|    |  \/  |  _ \
+ * | |     | |  | | | |  | | |   /   _  | |___  ___ | |\/| | |_) |
+ * | |     | |  | | | |  | | |  (   | | |  ___||___|| |  | |  __/
+ * | |_ _  | |__| | | |__| | |   \  | | | |____     | |  | | |
+ * |_____| |______| |______| |_|\_\ |_| |______|    |_|  |_|_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+*/
+
+namespace pocketmine\block;
+
+use pocketmine\item\Tool;
+
+class PurpleGlazedTerracotta extends Solid{
+
+	protected $id = self::PURPLE_GLAZED_TERRACOTTA;
+
+	public function __construct($meta = 0){
+		$this->meta = $meta;
+	}
+
+	public function getHardness(){
+		return 1.4;
+	}
+
+	public function getName(){
+		return "Purple Glazed Terracotta";
+	}
+
+	public function getToolType(){
+		return Tool::TYPE_PICKAXE;
+	}
+}
