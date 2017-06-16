171b40c5b7921d38a93865dc2ac9ee8fec1c7d40
linuzo <linuzo@kidzzone.ca>
2017-06-16 11:20:50 -0600

    chrousfruit fix

--- src/pocketmine/item/Item.php
+++ src/pocketmine/item/Item.php
@@ -231,6 +231,7 @@ class Item implements ItemIds, \JsonSerializable{
 			self::$list[self::PRISMARINE_CRYSTALS] = PrismarineCrystals::class;
 			self::$list[self::PRISMARINE_SHARD] = PrismarineShard::class;
 			self::$list[self::NETHER_STAR] = NetherStar::class;
+            self::$list[self::CHORUS_FRUIT] = ChorusFruit::class;
 			self::$list[self::ENCHANTED_GOLDEN_APPLE] = GoldenAppleEnchanted::class;
 			self::$list[self::ENDER_PEARL] = EnderPearl::class;
             
